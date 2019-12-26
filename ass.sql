4e0bde60b9b7b154e11e769229154f5fe8db6272
Nivetha.M <nivethamoorthy19@gmail.com>
2019-12-26 11:53:24 +0530

    Update ass.sql

--- ass.sql
+++ ass.sql
@@ -8,12 +8,12 @@ completed_date date,constraint status_check_cq check(status_check in('completed'
 insert into daily_work(status_check,work_name,work_by,deadline_date,completed_date)
 values('completed','installation','nandhu',to_date('27-12-2019','dd-MM-yyyy'),to_date('26-12-2019','dd-MM-yyyy');
 insert into daily_work(status_check,work_name,work_by,deadline_date,completed_date)
-values('completed','installation','sound',to_date('28-12-2019','dd-MM-yyyy'),to_date('27-12-2019','dd-MM-yyyy')                                                                                    
+values('completed','installation','sound',to_date('28-12-2019','dd-MM-yyyy'),to_date('27-12-2019','dd-MM-yyyy');                                                                                   
 select * from daily_work;
 
 alter table daily_work add(priority number);
 insert into daily_work(status_check,work_name,work_by,deadline_date,completed_date,priority)
 values('completed','installation','nandhu',to_date('27-12-2019','dd-MM-yyyy'),to_date('26-12-2019','dd-MM-yyyy',1);
 insert into daily_work(status_check,work_name,work_by,deadline_date,completed_date,priority)
-values('completed','installation','nandhu',to_date('27-12-2019','dd-MM-yyyy'),to_date('26-12-2019','dd-MM-yyyy',2)                                                                                     
+values('completed','installation','nandhu',to_date('27-12-2019','dd-MM-yyyy'),to_date('26-12-2019','dd-MM-yyyy',2);                                                                                   
 
